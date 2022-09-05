18dcd1e5c00370a8e4de0d6b98f09be951807ad3
eunshinNoh <nes0410@gmail.com>
2022-09-05 17:01:38 -0600

    refs #1 init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Main/>
+</template>
+
+<script>
+import Main from '@/views/main/Index.vue'
+export default {
+  name: 'App',
+  components:{
+    Main
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Source Sans Pro', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/main/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  Main
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
